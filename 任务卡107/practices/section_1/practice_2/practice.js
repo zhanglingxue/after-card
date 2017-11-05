7e6aa657bc985abdfe767d888763aecb6590d074
zhanglingxue <1050080567@qq.com>
2017-11-05 13:00:21 +0800

    更新

--- 任务卡107/practices/section_1/practice_2/practice.js
+++ 任务卡107/practices/section_1/practice_2/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var result = [];
+  for(var i = 0;i < collection_a.length;i ++) {
+    for(var j = 0;j < collection_b.length;j ++) {
+      for(var k = 0;k < collection_b[j].length;k ++) {
+        if(collection_a[i] === collection_b[j][k]) {
+          result.push(collection_a[i]);
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = collect_same_elements;
--- 任务卡107/practices/section_1/practice_4/practice.js
+++ 任务卡107/practices/section_1/practice_4/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var result = [];
+  var arr = object_b.value;
+  for(var i = 0;i < collection_a.length;i ++) {
+    for(var j = 0;j < arr.length;j ++) {
+      if(collection_a[i]['key'] === arr[j]) {
+        result.push(collection_a[i]['key']);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = collect_same_elements;
