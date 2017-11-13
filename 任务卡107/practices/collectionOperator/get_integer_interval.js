4855da9ce7790416063d935224b14f6531522cfe
zhanglingxue <1050080567@qq.com>
2017-11-14 00:07:46 +0800

    更新

--- 任务卡107/practices/collectionOperator/get_integer_interval.js
+++ 任务卡107/practices/collectionOperator/get_integer_interval.js
@@ -12,7 +12,7 @@ function get_integer_interval(number_a, number_b) {
         for(i = number_a;i >= number_b;i --) {
             arr.push(i);
         }
-    }else if(number_a == number_b) {
+    }else if(number_a ==== number_b) {
         arr.push(number_a);
     }
     return arr;
