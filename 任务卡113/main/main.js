d4f68a0396e5ff326a2815e49a77472a2ea3d1ac
张凌雪 <1050080567@qq.com>
2017-11-27 20:02:26 +0800

    完成113

--- 任务卡113/main/main.js
+++ 任务卡113/main/main.js
@@ -1,22 +1,24 @@
+function Time(time,s) {
+    if(time > 0) {
+        s += time * 0.25;
+        return s;
+    }
+}
 module.exports = function taxi(kilometre,time) {
     var kilo = Math.ceil(kilometre),
         t = Math.ceil(time);  //向上取整
     var sum = 6;
-    if(kilo <= 2) {
+    if(kilo <= 2) {//小于等于2公里（等待时间不计费）
         return sum;
-    }else if(kilo <= 8) {
+    }else if(kilo <= 8) {//不超过8公里
         kilo -= 2;
         sum += (kilo * 0.8);
-        if(time > 0) {
-            sum += t * 0.25;
-        }
+        sum = Time(t,sum);
         return parseInt(sum);
-    }else {
+    }else {//超过8公里
         kilo -= 8;
         sum += (6 * 0.8)+(kilo * 1.2);
-        if(time > 0) {
-            sum += t * 0.25;
-        }
+        sum = Time(t,sum);
         return parseInt(sum);
     }
 };
