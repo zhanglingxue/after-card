4bec5905ad64ded0210e381a9a95006bc6625d5e
张凌雪 <1050080567@qq.com>
2017-11-27 19:48:00 +0800

    完成113

--- 任务卡113/main/main.js
+++ 任务卡113/main/main.js
@@ -1,4 +1,22 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function taxi(kilometre,time) {
+    var kilo = Math.ceil(kilometre),
+        t = Math.ceil(time);  //向上取整
+    var sum = 6;
+    if(kilo <= 2) {
+        return sum;
+    }else if(kilo <= 8) {
+        kilo -= 2;
+        sum += (kilo * 0.8);
+        if(time > 0) {
+            sum += t * 0.25;
+        }
+        return parseInt(sum);
+    }else {
+        kilo -= 8;
+        sum += (6 * 0.8)+(kilo * 1.2);
+        if(time > 0) {
+            sum += t * 0.25;
+        }
+        return parseInt(sum);
+    }
 };
--- 任务卡113/spec/main-spec.js
+++ 任务卡113/spec/main-spec.js
@@ -1,5 +1,10 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    var kilometre = 13;
+    var time = 13;
+    it('出租车计价', function () {
+        const taxi = main(kilometre,time);
+        expect(taxi).toEqual(20);
+    })
 });
