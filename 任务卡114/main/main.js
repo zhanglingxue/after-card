874ec54b4eea016b933888e98e06080b878eec79
zhanglingxue <1050080567@qq.com>
2017-11-28 20:52:20 +0800

    完成114

--- 任务卡114/main/main.js
+++ 任务卡114/main/main.js
@@ -1,24 +1,60 @@
 // Write your cade below:
-function main(ans,num) {
-    var i = 0,
-        j = 0;
+
+//随机生成4位不相同的数
+function inTeger() {
+    var arr=[1,2,3,4,5,6,7,8,9,0];
+    var res=[];
+    var len=0;
+    var index=0;
+    for(var i=0;i<4;i++) {
+        len = arr.length;
+        index = Math.floor(Math.random()*(len-1));
+        //返回值：返回小于或等于x，并且与之最接近的整数
+        res.push(arr.splice(index,1)[0]);
+        //res += arr.splice(index,1);
+    }
+    return res;
+}
+
+//两数组数据比较
+function main(num) {
+    var ans = inTeger();
+    var nu = num;
+    var i , j ;
     var count = 0,
         sum = 0;
-    var end;
-    for(i;i < ans.length;i++) {
-        for(j;j < num.length;j++) {
-            if(ans[i] === num[j] && i === j) {
-                count += 1;
-            }else if(ans[i] === num[j] && i !== j) {
-                sum += 1;
+    for(i=0;i < ans.length;i++) {
+        for(j=0;j < nu.length;j ++) {
+            if(ans[i] === nu[j]) {
+                if(i === j){
+                    count += 1;
+                    break;
+                }else{
+                    sum += 1;
+                }
             }
         }
     }
-    end = count +'A'+ sum +'B';
-    console.log(end);
+    var end = count +'A'+ sum +'B';
+    console.log("随机数  :"+ans);
+    console.log("用户输入:"+nu);
+    console.log("返回值:"+end);
     return end;
 }
 
+//结果与测试事例比较
+function comPare(newArr,end) {
+    var no;
+    for(var i=0;i<newArr.length;i++) {
+        no = newArr[i];
+        if(end === no) {
+            break;
+        }
+        continue;
+    }
+    return no;
+}
+
 module.exports = {
-    main
+    main,comPare
 };
--- 任务卡114/spec/main-spec.js
+++ 任务卡114/spec/main-spec.js
@@ -2,19 +2,8 @@ const main = require('../main/main');
 
 
 describe('main()', () => {
-    var num = 9872;
-    var no;
-    var arr=[1,2,3,4,5,6,7,8,9,0];
-    var result='';
-    var len=0;
-    var index=0;
-    for(var i=0;i<4;i++) {
-        len = arr.length;
-        index = Math.floor(Math.random()*(len-1));
-        //返回值：返回小于或等于x，并且与之最接近的整数
-        result += arr.splice(index,1);
-    }
-    var newArr = [
+    var num = [9,8,7,2]; //用户输入
+    var newArr = [    // 14种可能的结果
         4+'A'+0+'B',
         3+'A'+0+'B',
         2+'A'+2+'B',
@@ -32,17 +21,10 @@ describe('main()', () => {
     ];
 
     it('Guess Number',function () {
-        let res = main.main(result,num);
-        for(var i=0;i<newArr.length;i++) {
-            no = newArr[i];
-            if(res === no) {
-                break;
-            }
-            continue;
-        }
-        console.log(result);
-        console.log(num);
-        console.log(no);
+        // let math = main.inTeger();
+        let res = main.main(num);
+        let no = main.comPare(newArr,res);
+
         expect(res).toEqual(no);
     })
 });
