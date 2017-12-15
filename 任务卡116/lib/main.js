3562f6af45398682b09eeb18b560ad4272d1e6cf
张凌雪 <1050080567@qq.com>
2017-12-15 16:22:25 +0800

    更新116

--- 任务卡116/lib/main.js
+++ 任务卡116/lib/main.js
@@ -1,2 +1,21 @@
-module.exports = () => {
+function Con() {
+     console.log(`1. 添加学生
+2. 生成成绩单
+3. 退出
+请输入你的选择（1～3）：`);
 }
+
+function Main(num) {
+    if(num === 1){
+
+    }
+    if(num === 2){
+
+    }
+    if(num === 3){
+        return;
+    }
+}
+
+
+module.exports = {Main,Con};
--- 任务卡116/spec/main-spec.js
+++ 任务卡116/spec/main-spec.js
@@ -2,14 +2,24 @@ let sinon = require("sinon");
 let main = require("../lib/main");
 
 describe('main()', () => {
-
     it('should display main menu once started', () => {
         sinon.spy(console, 'log');
-        main();
+        main.Con();
         expect(console.log.args.join()).toBe(`1. 添加学生
 2. 生成成绩单
 3. 退出
 请输入你的选择（1～3）：`);
     });
-
+    it('chance 1',() =>{
+        main.Main(1);
+        console.log('学生'+'的成绩被添加');
+    });
+    it('chance 1',() =>{
+        main.Main(2);
+        expect().toEqual();
+    });
+    it('chance 1',() =>{
+        main.Main(3);
+        // expect().toEqual();
+    })
 });
