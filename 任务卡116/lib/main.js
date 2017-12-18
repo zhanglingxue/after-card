7d51f4b46d9a41b08d355635968b89238e2b1705
张凌雪 <1050080567@qq.com>
2017-12-18 20:26:16 +0800

    更新116

--- 任务卡116/lib/main.js
+++ 任务卡116/lib/main.js
@@ -1,21 +1,21 @@
-function Con() {
+function Main() {
      console.log(`1. 添加学生
 2. 生成成绩单
 3. 退出
 请输入你的选择（1～3）：`);
-}
 
-function Main(num) {
-    if(num === 1){
+     let scanf=require('scanf');
+     let str=scanf("%d");
+     if(str === 1) {
+         console.log('请输入学生信息（格式：姓名, 学号, 民族, 班级, 学科: 成绩, ...），按回车提交:');
+         let input = scanf('%s');
+         console.log(input);
+     }
 
-    }
-    if(num === 2){
-
-    }
-    if(num === 3){
-        return;
-    }
 }
 
 
-module.exports = {Main,Con};
+
+
+
+module.exports = {Main};
